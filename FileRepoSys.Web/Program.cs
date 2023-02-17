a87a69337e509778c2bc27be14da66e99df7b0c6
xqqqq <40887052+csysm@users.noreply.github.com>
2023-02-17 22:42:09 +0800

    New feature (#6)
    * 添加验证码

--- FileRepoSys.Web/Program.cs
+++ FileRepoSys.Web/Program.cs
@@ -9,6 +9,7 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://43.140.215.157/api/") });
+//http://43.140.215.157/api/
 builder.Services.AddAuth();
 builder.Services.AddBlazoredLocalStorage();
 builder.Services.AddAntDesign();
